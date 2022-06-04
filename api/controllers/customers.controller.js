40e29cc1e67b3759b039796dcef6fd81152ea33a
David Pallarés <serallapdivad@gmail.com>
2022-06-04 17:57:58 +0100

    # Controllers - getAllCustomers is working

--- api/controllers/customers.controller.js
+++ api/controllers/customers.controller.js
@@ -2,24 +2,33 @@ const CustomerModel = require('../models/customer.model')
 
 async function createCustomer(req, res) {
     try {
-        const customer = await CustomerModel.create(req.body)
+        const customer = await CustomerModel.create(req.body, { _id: 0, __v: 0 })
         res.status(200).json(customer);
-    
     } catch (error) {
-        res.status(500).send(`Error creating user: ${error}`);
+        res.status(500).send(`Error creating customer: ${error}`);
     }
 }
 
 async function getSingleCustomer(req, res) {
     try {
-        const customer = await CustomerModel.findById(req.params.id)
+        const customer = await CustomerModel.findById(req.params.id, { _id: 0, __v: 0 })
         res.status(200).json(customer);
     } catch (error) {
-        res.status(500).send(`Error obtaining user: ${error}`);
+        res.status(500).send(`Error obtaining customer: ${error}`);
+    }
+}
+
+async function getAllCustomers(req, res) {
+    try {
+        const customers = await CustomerModel.find(req.query, { _id: 0, __v: 0 })
+        res.status(200).json(customers)
+    } catch (error) {
+        res.status(500).send(`Couldn't get all customers ${error}`)
     }
 }
 
 module.exports = {
     createCustomer,
-    getSingleCustomer
+    getSingleCustomer,
+    getAllCustomers
 }
--- api/routes/customers.router.js
+++ api/routes/customers.router.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const {
   createCustomer,
-  getSingleCustomer
+  getSingleCustomer,
+  getAllCustomers
 } = require("../controllers/customers.controller");
 
 router
   .post("/", createCustomer)
   .get("/:id", getSingleCustomer)
+  .get("/", getAllCustomers)
 
 module.exports = router;
